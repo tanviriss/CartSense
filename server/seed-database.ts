aece0a67ba6045b39f7e14aef25116abbcddb437
Tanvir <Tanvirislamnyc@gmail.com>
2025-08-30 20:56:54 -0400

    defining schemas using zod

--- server/seed-database.ts
+++ server/seed-database.ts
@@ -12,3 +12,30 @@ const llm = new ChatGoogleGenerativeAI({
   temperature: 0.7,
   apiKey: process.env.GOOGLE_API_KEY
 })
+
+const itemSchema = z.object({
+  item_id: z.string(),
+  item_name: z.string(),
+  item_description: z.string(),
+  brand: z.string(),
+  manufacturer_address: z.object({
+    street: z.string(),
+    city: z.string(),
+    state: z.string(),
+    postal_code: z.string(),
+    country: z.string()
+  }),
+  prices: z.object({
+    full_price: z.number(),
+    sale_price: z.number(),
+  }),
+  categories: z.array(z.string()),
+  user_reviews: z.array(
+    z.object({
+      review_date: z.string(),
+      rating: z.number(),
+      comment: z.string(),
+    })
+  ),
+  notes: z.string()
+})
